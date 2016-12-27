35b2bc6a4df43982c733cdca24e12fc548f92185
jiyee <jiyee.sheng@gmail.com>
2016-12-27 10:43:25 +0800

    MOD: 增加 sitemap 和 百度统计代码

---
+++ themes/apollo/layout/partial/baidu-hmt.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?ba828c541dc9447a1fbbb5910973873d";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
